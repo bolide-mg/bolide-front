04a79494cb9375e5cd58d36190ed221029ede94b
tendryAxel <hei.tendry@gmail.com>
2024-05-25 11:28:11 +0300

    feat: fontawesome

--- app/car/layout.tsx
+++ app/car/layout.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 import './style.scss'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEllipsis} from "@fortawesome/free-solid-svg-icons";
 
 const Main = ({ children } : { children: React.ReactNode }) => {
     return (
         <div className="layout">
             <div className="header">
-                Header
+                <button type="button">
+                    <FontAwesomeIcon icon={faEllipsis} width={30} />
+                </button>
+            </div>
+            <div className="menu">
+                menu
             </div>
             <div className="main">
                 {children}
