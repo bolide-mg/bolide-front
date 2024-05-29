1c99a4d7e6c9ece9e08c9873c298f1d9d694a30a
tendryAxel <hei.tendry@gmail.com>
2024-05-29 12:52:52 +0300

    feat: implement /admin login method

--- Provider/authProvider.ts
+++ Provider/authProvider.ts
@@ -1,13 +1,35 @@
+import {encodeToBase64} from "next/dist/build/webpack/loaders/utils";
+
 export const authProvider = {
     login: ({ username, password } : { username: string, password: string }) => {
-        if (username !== 'john' || password !== '123') {
-            return Promise.reject();
-        }
+        const request = new Request('http://localhost:8080/admin/token', {
+            method: 'POST',
+            headers: new Headers({
+                Authorization: `Basic ${encodeToBase64(`${username}:${password}`)}`,
+            })
+        });
+        const result = fetch(request)
+            .then(response => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.statusText);
+                }
+                const result = response.json();
+                console.log(result);
+                return result;
+            })
+            .then(auth => {
+                localStorage.setItem('auth', JSON.stringify(auth));
+            })
+            .catch(e => {
+                console.error(e);
+                throw new Error('Network error')
+            });
         localStorage.setItem('username', username);
         return Promise.resolve();
     },
     logout: () => {
         localStorage.removeItem('username');
+        localStorage.removeItem('auth');
         return Promise.resolve();
     },
     checkAuth: () =>
