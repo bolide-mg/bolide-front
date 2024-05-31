3796bec525062745309c79205bf02e5b48ca0f42
tendryAxel <hei.tendry@gmail.com>
2024-05-31 15:27:15 +0300

    chore: about page template

--- app/car/[id]/page.tsx
+++ app/car/[id]/page.tsx
@@ -1,6 +1,16 @@
 const Main = () => {
+    //TODO: Fill this about components with the about XD... of a car
+
     return (
-        <>Id</>
+        <>
+            <div className="w-full h-full p-8 text-text-base">
+                <div className="flex w-full justify-evenly">
+                    <div className="h-96 w-4/5 flex items-center justify-center bg-base rounded-xl">image</div>
+                    <div className="w-52 bg-base flex flex-col items-center p-8 rounded">actions</div>
+                </div>
+                <div>description</div>
+            </div>
+        </>
     )
 }
 
