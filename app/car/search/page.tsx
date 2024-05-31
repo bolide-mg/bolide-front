b564d6fa07a45c45ed1f21cb592ea64f94a3cff8
tendryAxel <hei.tendry@gmail.com>
2024-05-31 13:38:34 +0300

    chore: search page

--- app/car/search/page.tsx
+++ app/car/search/page.tsx
@@ -1,6 +1,23 @@
+import {Input} from "@headlessui/react";
+
 const Main = () => {
     return (
-        <>Search</>
+        <>
+            <div className="w-full h-full bg-white">
+                <div className="flex w-full h-16 items-center justify-center">
+                    <Input
+                        type="text"
+                        placeholder="search"
+                        className="rounded-md border h-3/4 p-1"
+                    />
+                </div>
+                <div className="min-h-full flex justify-center py-5">
+                    <div className="w-full md:w-3/4 rounded-xl flex flex-col border">
+                        The result
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
