65eca01122a414ebc6c3302c739da43ba9ed53fe
tendryAxel <hei.tendry@gmail.com>
2024-06-01 09:53:56 +0300

    feat: wrapper (axios to backend endpoint)

---
+++ axios/car.ts
@@ -0,0 +1,17 @@
+import instance from "@/axios/default";
+import axios from "axios";
+import {Car} from "@/axios/model/Car";
+
+const carAxios = axios.create(instance.defaults)
+carAxios.defaults.baseURL += "/car"
+
+export const getAllCar = (): Promise<Car[]> => carAxios.get("")
+    .then(r=>r.data);
+
+export const getCarById = (id: typeof Car.prototype.id): Promise<Car> => carAxios.get(`/${id}`)
+    .then(r=>r.data);
+
+export const postCar = (car: Car): Promise<Car> => carAxios.post("", car)
+    .then(r=>r.data);
+
+export default carAxios;
---
+++ axios/model/Appointment.ts
@@ -0,0 +1,23 @@
+export class Appointment{
+    id: number;
+    name: string;
+    first_name: string;
+    email: string;
+    message: string;
+    contact: string;
+    appointment_date: Date;
+    status: string;
+    id_car: number;
+
+    constructor(id: number, name: string, first_name: string, email: string, message: string, contact: string, appointment_date: Date, status: string, id_car: number) {
+        this.id = id;
+        this.name = name;
+        this.first_name = first_name;
+        this.email = email;
+        this.message = message;
+        this.contact = contact;
+        this.appointment_date = appointment_date;
+        this.status = status;
+        this.id_car = id_car;
+    }
+}
