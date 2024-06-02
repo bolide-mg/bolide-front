5ed0dceadea9b1652640b126ba4aaa016311835e
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2024-06-02 19:19:40 +0300

    refactor: hero section with image

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,45 @@
 import { NextPage } from "next";
-import Link from 'next/link'
+import Link from 'next/link';
+import Image from 'next/image';
+import heroImage from '@/assets/img/2024-AMG-CLE-COUPE.webp';
 
 const Home: NextPage = () =>
     <>
-        <nav className="px-4 py-2 flex justify-between align-center bg-[#141414] bg-opacity-25 backdrop-blur overflow-hidden fixed w-full shadow-2xl">
-            <span className="text-3xl font-bold text-[#EBEBEB]">bolide.mg</span>
+        <nav className="px-32 py-2 flex justify-between align-center bg-dark bg-opacity-50 backdrop-blur overflow-hidden fixed w-full shadow-2xl z-20">
+            <Link href="/"><span className="text-3xl font-bold text-light">bolide.mg</span></Link>
             <Link href="/admin"><button className="button-primary">Se connecter</button></Link>
         </nav>
-        <main className="pt-12 bg-[#fcbf49]">
-            <div id="hero" className="h-screen p-4 flex flex-col items-center">
-                <h1>Votre concessionnaire automobile préféré</h1>
+        <main className="px-32 py-8 font-medium">
+            <div id="hero" className="h-lvh flex flex-col items-center justify-center">
+                <Image src={heroImage} priority width={2400} height={900} alt="2024-AMG-CLE-COUPE" placeholder="blur" className="-z-10 h-screen absolute overflow-hidden aspect-square top-0" />
+                <div className="drop-shadow text-light flex flex-col px-12 gap-6">
+                    <div>
+                        <h1>Le concessionnaire de vos voitures de sport</h1>
+                        <p>depuis 2005</p>
+                    </div>
+                    <div className="flex flex-row gap-4">
+                        <Link href="/"><button className="button-primary">Prendre rendez-vous</button></Link>
+                        <Link href="/admin"><button className="button-primary">Se connecter</button></Link>
+                    </div>
+                </div>
             </div>
-            <div id="catalogue" className="h-screen p-4 bg-[#d62828]">
+            <div id="catalogue" className="h-screen py-12 flex flex-col gap-8 text-center">
                 <h1>Notre catalogue</h1>
                 <div id="brands">
                     <h2>Nos marques</h2>
-                    <p>Mazda, Toyota, GMC</p>
+                    <p>Mazda, Toyota, GMC, Citroen, Mercedes Benz AMG, Porsche</p>
                 </div>
                 <div id="carShow">
                     <h2>Car Show</h2>
+                    <div>TODO: Carousel with car cards</div>
                 </div>
             </div>
         </main>
-        <footer className="h-[60vh] p-4 flex flex-row gap-4 bg-[#f77f00]">
+        <footer className="h-screen px-32 py-24 flex flex-row gap-8 bg-dark text-light font-medium">
             <div>
-                <h1>Contact</h1>
+                <h1>Nous contacter</h1>
                 <div className="m-auto flex flex-col gap-2">
-                    <p>Mail : <a href="#" >hello@bolide.mg</a></p>
+                    <p>Mail : hello@bolide.mg</p>
                     <p>Telephone : +261 32 00 000 80</p>
                     <p>Adresse : LOT IVE 92 Behoririka, Antananarivo 101, Madagascar BP3779</p>
                     <div>
@@ -36,7 +49,8 @@ const Home: NextPage = () =>
                 </div>
             </div>
             <div>
-                <h1>Nous contacter</h1>
+                <h1>Prendre rendez-vous</h1>
+                <div>TODO: forms</div>
             </div>
         </footer>
     </>
