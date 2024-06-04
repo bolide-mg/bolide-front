bf2141ea312bc4d8dab0596c16de64e40a1dc6dd
tendryAxel <hei.tendry@gmail.com>
2024-06-04 11:19:06 +0300

    feat: display list of search result

--- app/car/search/page.tsx
+++ app/car/search/page.tsx
@@ -1,9 +1,16 @@
+"use client"
+
 import {Input} from "@headlessui/react";
+import SearchResult from "@/components/SearchResult";
+import {useState} from "react";
+import {Car} from "@/axios/model/Car";
 
 const Main = () => {
+    const [resultCar, setResultCar] = useState<Car[]>([Car.prototype])
+
     return (
         <>
-            <div className="w-full h-full bg-white">
+            <div className="w-full min-h-full bg-white">
                 <div className="flex w-full h-16 items-center justify-center">
                     <Input
                         type="text"
@@ -12,9 +19,12 @@ const Main = () => {
                     />
                 </div>
                 <div className="min-h-full flex justify-center py-5">
-                    <div className="w-full md:w-3/4 rounded-xl flex justify-evenly p-5 border flex-wrap gap-10">
-                        The result
-                        {/*TODO: design the search page result*/}
+                    <div className="min-h-96 w-full md:w-3/4 rounded-xl flex items-center border flex-col overflow-hidden">
+                        {
+                            resultCar.length == 0 ?
+                                "no result" :
+                                resultCar.map(car => <SearchResult key={Math.random()} />)
+                        }
                     </div>
                 </div>
             </div>
