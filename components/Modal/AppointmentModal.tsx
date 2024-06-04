fa254003c2698ff9988cdcfa39abda3a808e6c9a
tendryAxel <hei.tendry@gmail.com>
2024-06-04 18:54:12 +0300

    chore: appointment form in modal

--- components/Modal/AppointmentModal.tsx
+++ components/Modal/AppointmentModal.tsx
@@ -2,6 +2,7 @@
 
 import Modal from "@/components/Modal/index";
 import React, { useState } from "react";
+import AppointmentForm from "@/components/AppointmentForm";
 
 const Main = () => {
   const [show, setShow] = useState(false);
@@ -22,63 +23,7 @@ const Main = () => {
         Book
       </button>
       <Modal onClose={onCloseModal} show={show}>
-        <form
-          onSubmit={onSubmit}
-          className="flex flex-col justify-between h-full pb-20 pl-4 md:pl-14 overflow-y-auto gap-5"
-        >
-          <h3 className="text-xl font-bold">Create appointment</h3>
-          <div>
-            <div className="mb-2 block">
-              <p>Name</p>
-            </div>
-            <input
-              type="text"
-              placeholder="John"
-              className="h-10 rounded p-4 ring-grey ring-1"
-            />
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <p>First Name</p>
-            </div>
-            <input
-              type="text"
-              placeholder="Doe"
-              className="h-10 rounded p-4 ring-grey ring-1"
-            />
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <p>Email</p>
-            </div>
-            <input
-              type="email"
-              placeholder="test@gmail.com"
-              className="h-10 rounded p-4 ring-grey ring-1"
-            />
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <p>Date</p>
-            </div>
-            <input type="date" className="h-10 rounded p-4 ring-grey ring-1" />
-          </div>
-          <div className="flex p-6 gap-6">
-            <button
-              type="submit"
-              className="bg-primary w-24 h-8 rounded text-light"
-            >
-              Confirm
-            </button>
-            <button
-              type="button"
-              className="bg-error w-24 h-8 rounded text-light"
-              onClick={onCloseModal}
-            >
-              Close
-            </button>
-          </div>
-        </form>
+        <AppointmentForm />
       </Modal>
     </>
   );
