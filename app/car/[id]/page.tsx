9622d8959932157fa7dd4915bae6238f6631752b
tendryAxel <hei.tendry@gmail.com>
2024-06-05 13:57:42 +0300

    feat: some description in about

--- app/car/[id]/page.tsx
+++ app/car/[id]/page.tsx
@@ -1,12 +1,14 @@
 import AppointmentModal from "@/components/Modal/AppointmentModal";
 import { FC } from "react";
+import {Car} from "@/axios/model/Car";
 
 const Main: FC<{ params: { id: number } }> = ({ params }) => {
   //TODO: Fill this about components with the about XD... of a car
+  const car = Car.prototype;
 
   return (
     <>
-      <div className="w-full h-full p-8 text-text-base">
+      <div className="w-full min-h-full p-8 text-text-base">
         <div className="flex w-full justify-evenly">
           <div className="h-96 w-4/5 flex items-center justify-center bg-light rounded-xl">
             image
@@ -14,14 +16,30 @@ const Main: FC<{ params: { id: number } }> = ({ params }) => {
           <div className="w-52 bg-light flex flex-col items-center p-8 gap-8 rounded">
             <p className="font-bold">This car</p>
             <div>
-              <p>Price: </p><p className="font-mono">123.00€</p>
+              <p>Price: </p><p className="font-mono">{car.price | 0}€</p>
             </div>
             <div className="w-full border border-dark"></div>
             <p className="font-bold">Actions</p>
             <AppointmentModal idCar={params.id}/>
           </div>
         </div>
-        <div>description</div>
+        <div className="p-8">
+          <p className="font-bold">Description</p>
+          <p className="">Name: {car.name}</p>
+          <p className="">#{car.id}</p>
+          <p className="">Description</p>
+          <p>
+            {car.description}
+          </p>
+          <p className="">Brand: {car.brand}</p>
+          <p className="">Model: {car.model}</p>
+          <p className="">Color: {car.color}</p>
+          <p className="">Motor Type: {car.motor_type}</p>
+          <p className="">Power: {car.power}</p>
+          <p className="">Place Number: {car.place_number}</p>
+          <p className="">Status: {car.status}</p>
+          <p className="">Type: {car.type}</p>
+        </div>
       </div>
     </>
   );
