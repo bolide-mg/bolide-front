691c6275c14c151d4f65f1180d423fec24483361
tendryAxel <hei.tendry@gmail.com>
2024-06-10 14:01:07 +0300

    fix: cannot send email from another account

--- axios/email.ts
+++ axios/email.ts
@@ -4,7 +4,7 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const sendEmail = async (content: string, emailSender: string = 'onboarding@resend.dev', emailToSend: string | undefined = process.env.EMAIL_ADMIN) => {
+export const sendEmail = async (content: string, emailToSend: string | undefined = process.env.EMAIL_ADMIN, emailSender: string = 'onboarding@resend.dev') => {
   if (!emailToSend) {
     throw new Error('Email is null, so set env EMAIL_ADMIN');
   }
