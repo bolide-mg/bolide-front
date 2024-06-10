9aaee38e68862517828b9b08531aa61c7c8f2386
tendryAxel <hei.tendry@gmail.com>
2024-06-10 13:45:53 +0300

    fix: email function

--- axios/email.ts
+++ axios/email.ts
@@ -4,13 +4,12 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const sendEmail = async (content: string) => {
-  const emailToSend = process.env.EMAIL_ADMIN;
+export const sendEmail = async (content: string, emailSender: string = 'onboarding@resend.dev', emailToSend: string | undefined = process.env.EMAIL_ADMIN) => {
   if (!emailToSend) {
     throw new Error('Email is null, so set env EMAIL_ADMIN');
   }
   return resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: emailSender,
     to: emailToSend || '',
     subject: "Appointment created",
     html: `<p>The appointment ${content}</p>`,
